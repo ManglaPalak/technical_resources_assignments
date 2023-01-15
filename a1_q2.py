442a7c89a35a7b3afa59ff13afc20e10312658a5
ManglaPalak <122383012+ManglaPalak@users.noreply.github.com>
2023-01-15 15:10:42 +0530

    submitting q2 of assignment 1

---
+++ a1_q2.py
@@ -0,0 +1,11 @@
+marks=int(input('enter the marks of the student:'))
+if marks>90:
+    print('excellent')
+elif marks>80:
+    print('good')
+elif marks>70:
+    print('fair')
+elif marks>60:
+    print('meets expectations')
+else:
+    print('below par')
